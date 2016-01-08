f523b7d65cffbc0c4635c8382234dc18d1d24a1a
patrickmontalto <pmontalto@gmail.com>
2016-01-08 16:54:22 -0500

    initial js

---
+++ calc.js
@@ -0,0 +1,23 @@
+// Core arithmetic functions
+
+var add = function(x, y) {
+	return (x + y);
+}
+
+var multiply = function(x, y) {
+	return (x * y);
+}
+
+var divide = function(x, y) {
+	return (x / y);
+}
+
+var subtract = function(x, y) {
+	return ( x - y);
+}
+
+// Event listeners for number buttons (0-9):
+
+// Event listeners for operations buttons (+, -, %, x, =, clc)
+
+
